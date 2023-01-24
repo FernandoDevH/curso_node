5aedcb1976f01f1b77ac839dc5959e8838140bdb
Fernando Henrique <103213962+FernandoDevH@users.noreply.github.com>
2023-01-24 16:05:42 -0300

    Aula-7

---
+++ 1_FUNDAMENTOS/7_treinando_via_console/index.js
@@ -0,0 +1,26 @@
+// contagem de impressoões
+const x = 10
+const y = 'Fernando'
+
+
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+
+
+// variavel entre string
+
+console.log('Olá %s',y)
+
+//Limpar o console automaticamente
+
+setTimeout(() => {
+    console.clear()
+},2000)
